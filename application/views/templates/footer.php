9b21b25559ee0848cae024d9338687b499609ccd
Stefan Halus <stefanhalus@gmail.com>
2018-07-18 23:58:46 +0300

    Footer updated

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,12 @@
-<em>&copy; <?php echo date('Y'); ?></em>
+<p>&nbsp;</p>
+<p class="centered"><em>&copy; <?php echo date('Y'); ?> eCabCardio - internship project mentored by <a target="_new" href="https://qinsoft.ro/">Qinsoft Solucions</a></em></p>
+
+<script src="<?php echo base_url();?>appearance/js/jquery-3.3.1.min.js" media="screen"></script>
+<script src="<?php echo base_url();?>appearance/js/animsition.js" media="screen"></script>
+
+<script src="<?php echo base_url();?>appearance/js/bootstrap.min.js" media="screen"></script>
+
+<script src="<?php echo base_url();?>appearance/js/sticky-header.js" media="screen"></script>
+
 </body>
-<script src="<?php echo base_url();?>/appearance/js/.js" media="screen"></script>
 </html>
